4e7984b43102be9095781f08b3dcf26d5ceb5493
Anurag Suthar <anuragsuthar2002@gmail.com>
2025-02-06 21:50:46 +0530

    Add Prettier configuration for consistent line endings in ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ]
   },
 };
