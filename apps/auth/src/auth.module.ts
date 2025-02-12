97e4356d1356d42c4e98225bd8fb13ee4e30c02d
Anurag Suthar <anuragsuthar2002@gmail.com>
2025-02-12 11:14:23 +0530

    Implement JWT authentication and microservices integration in Auth and Reservations modules; refactor configuration and enhance user handling

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -19,7 +19,8 @@ import { JwtStrategy } from './strategies/jwt.strategy';
         JWT_SECRET: Joi.string().required(),
         MONGODB_URI: Joi.string().required(),
         JWT_EXPIRATION: Joi.string().required(),
-        PORT: Joi.number().required(),
+        HTTP_PORT: Joi.number().required(),
+        TCP_PORT: Joi.number().required(),
       }),
     }),
     JwtModule.registerAsync({
--- libs/common/src/decorators/current.user.decorator.ts
+++ libs/common/src/decorators/current.user.decorator.ts
@@ -1,6 +1,6 @@
 import { ExecutionContext } from '@nestjs/common';
 import { createParamDecorator } from '@nestjs/common';
-import { UserDocument } from './users/models/user.schema';
+import { UserDocument } from '../../../../apps/auth/src/users/models/user.schema';
 
 const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
   return context.switchToHttp().getRequest().user;
